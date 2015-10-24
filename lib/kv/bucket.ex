e40687e593bf447e97839768c1cc75dfbfa08c43
Leigh Halliday <leighhalliday@gmail.com>
2015-10-24 15:29:28 -0400

    KV bucket with GenServer and GenEvent

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,29 @@
+defmodule KV.Bucket do
+  @doc """
+  Start a new `bucket`.
+  """
+  def start_link do
+    Agent.start_link(fn -> HashDict.new end)
+  end
+
+  @doc """
+  Get a `value` from the `bucket` using a `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &HashDict.get(&1, key))
+  end
+
+  @doc """
+  Put a `value` into the `bucket` for a `key`.
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &HashDict.put(&1, key, value))
+  end
+
+  @doc """
+  Removes a `key` from the `bucket`.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &HashDict.pop(&1, key))
+  end
+end
